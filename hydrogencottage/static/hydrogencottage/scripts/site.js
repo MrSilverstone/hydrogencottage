78ed96c6d4eab5ddf2f07360f0fd2a42a4bd82f4
Louis Mondesir <louis.mondesir@gmail.com>
2017-03-31 16:06:42 +0900

    Update design

--- hydrogencottage/static/hydrogencottage/scripts/site.js
+++ hydrogencottage/static/hydrogencottage/scripts/site.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     initOnePage();
-    introAnim();
-
     $("#section-accueil").parallax("50%", 0.1);
     $("#section-mission").parallax("75%", 0.2);
     $("#section-pitch").parallax("50%", 0.2);
@@ -12,29 +10,17 @@ $(document).ready(function () {
 });
 
 function initOnePage() {
-    $('#fullpage').fullpage({
-        verticalCentered: true,
-        sectionsColor: ['#4db7f9', '#a822c1', '#21a6b3', '#9c3e50', '€4ef5d1', '#ff2266', '#a2c130'],
-        anchors: ['accueil', 'mission', 'pitch', 'experimentation', 'village', 'partenaires', 'contact'],
-        menu: '#menu-right',
-        navigation: true,
-        navigationPosition: 'right',
-        navigationTooltips: ['Accueil', 'Mission', 'Le pitch', "L'expérimentation", 'Le village', 'Les partenaires', 'Contact'],
-        responsiveWidth: 900,
-        scrollBar: true,
-        afterResponsive: function (isResponsive) {
-
+    $('#fullpage').fullpage(
+        {
+            sectionsColor: ['#4db7f9', '#a822c1', '#21a6b3', '#9c3e50', '€4ef5d1', '#ff2266', '#a2c130'],
+            anchors: ['accueil', 'mission', 'pitch', 'experimentation', 'village', 'partenaires', 'contact'],
+            menu: '#menu-right',
+            navigation: true,
+            navigationPosition: 'right',
+            navigationTooltips: ['Accueil', 'Mission', 'Le pitch', "L'expérimentation", 'Le village', 'Les partenaires', 'Contact'],
+            responsiveWidth: 900,
+            scrollBar: true,
+            fixedElements: '#fixed-header',
         }
-    });
-}
-
-function introAnim() {
-    setTimeout(() => {
-        $("#accueil-title").fadeIn(2000, () => {
-            setTimeout(() => {
-                $("#hydrogen-pres").fadeIn(2000);
-            }, 0);
-        });
-    }, 1000);
-
+    );
 }

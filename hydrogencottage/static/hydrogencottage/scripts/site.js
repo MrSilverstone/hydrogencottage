9db3c2cabf7fce1bb0db89f42e5634112e3e91fc
Louis Mondesir <louis.mondesir@gmail.com>
2017-04-03 13:40:39 +0900

    Responsive menu

--- hydrogencottage/static/hydrogencottage/scripts/site.js
+++ hydrogencottage/static/hydrogencottage/scripts/site.js
@@ -24,3 +24,24 @@ function initOnePage() {
         }
     );
 }
+
+$(function() {
+	menu = $('nav ul');
+
+  $('#openup').on('click', function(e) {
+    e.preventDefault(); menu.slideToggle();
+  });
+  
+  $(window).resize(function(){
+    var w = $(this).width(); if(w > 1280 && menu.is(':hidden')) {
+      menu.removeAttr('style');
+    }
+  });
+  
+  $('nav li').on('click', function(e) {                
+    var w = $(window).width(); if(w < 1280 ) {
+      menu.slideToggle(); 
+    }
+  });
+  $('.open-menu').height($(window).height());
+});
